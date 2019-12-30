6a10d2b1a6ff9f091b36814d62ce99ded7d6376d
Diego Araujo <diego_shakan@hotmail.com>
2019-12-30 12:06:43 -0300

    mudei o nome da varíavel que recebe o resultado

--- AtvIMC/assets/js/main.js
+++ AtvIMC/assets/js/main.js
@@ -9,22 +9,22 @@ function imc(){
         const altura = form.querySelector('.altura');
         let resultado = peso.value / altura.value**2
         resultado = resultado.toFixed(2)
-        let comparaResultado;
+        let recebeResultado;
         if (resultado < 18.5) {
-            comparaResultado = `Seu imc ${resultado} e você está abaixo do peso`
+            recebeResultado = `Seu imc ${resultado} e você está abaixo do peso`
         } else if (resultado >= 18.5 && resultado < 25) {
-            comparaResultado = `Seu imc ${resultado} e você está com peso normal`
+            recebeResultado = `Seu imc ${resultado} e você está com peso normal`
         } else if (resultado >= 25 && resultado < 30) {
-            comparaResultado = `Seu imc ${resultado} e você está com sobrepeso`
+            recebeResultado = `Seu imc ${resultado} e você está com sobrepeso`
         } else if (resultado >= 30 && resultado < 35) {
-            comparaResultado = `Seu imc ${resultado} e você está com obesidade grau 1`
+            recebeResultado = `Seu imc ${resultado} e você está com obesidade grau 1`
         } else if (resultado >=35 && resultado < 40) {
-            comparaResultado = `Seu imc ${resultado} e você está com obesidade grau 2`
+            recebeResultado = `Seu imc ${resultado} e você está com obesidade grau 2`
         } else if (resultado >= 40) {
-            comparaResultado = `Seu imc ${resultado} e você está com obesidade grau 3`
+            recebeResultado = `Seu imc ${resultado} e você está com obesidade grau 3`
         }
 
-        retorno.innerHTML = `<p>${comparaResultado}</p>`
+        retorno.innerHTML = `<p>${recebeResultado}</p>`
     };    
 
     form.addEventListener('submit', getForm);
