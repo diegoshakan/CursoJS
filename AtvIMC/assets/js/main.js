2b3b6ef4b8fb660706491af1ad58bfb9d1f02461
Diego Araujo <diego_shakan@hotmail.com>
2019-12-30 11:57:00 -0300

    atividade realizada com êxito

---
+++ AtvIMC/assets/js/main.js
@@ -0,0 +1,33 @@
+function imc(){
+    const form = document.querySelector('.form')
+    const retorno = document.querySelector('.retorno')
+
+    function getForm(evento){
+        evento.preventDefault();
+
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+        let resultado = peso.value / altura.value**2
+        resultado = resultado.toFixed(2)
+        let comparaResultado;
+        if (resultado < 18.5) {
+            comparaResultado = `Seu imc ${resultado} e você está abaixo do peso`
+        } else if (resultado >= 18.5 && resultado < 25) {
+            comparaResultado = `Seu imc ${resultado} e você está com peso normal`
+        } else if (resultado >= 25 && resultado < 30) {
+            comparaResultado = `Seu imc ${resultado} e você está com sobrepeso`
+        } else if (resultado >= 30 && resultado < 35) {
+            comparaResultado = `Seu imc ${resultado} e você está com obesidade grau 1`
+        } else if (resultado >=35 && resultado < 40) {
+            comparaResultado = `Seu imc ${resultado} e você está com obesidade grau 2`
+        } else if (resultado >= 40) {
+            comparaResultado = `Seu imc ${resultado} e você está com obesidade grau 3`
+        }
+
+        retorno.innerHTML = `<p>${comparaResultado}</p>`
+    };    
+
+    form.addEventListener('submit', getForm);
+}
+
+imc()
